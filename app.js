da0325f35a0db5d1259cf2b144cf4ef94fa8ec23
gilbert-kiana <gilbertintabo@gmail.com>
2022-07-13 15:05:46 +0300

    few changes in packages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("This is a response from https");
+  if (req.url === "/") res.write("This is a response from https");
   res.end();
 });
 
