5f0abd8801803ab70673ad0463684767d222a4f8
gilbert-kiana <gilbertintabo@gmail.com>
2022-07-15 12:01:44 +0300

    event loop,promises and async await

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") res.write("This is a response from https");
-  res.end();
+  if (req.url === "/") {
+    res.end("Home page");
+  }
+  if (req.url === "/about") {
+    res.end("About Page");
+  }
+  res.end("ERrop page");
 });
 
-server.listen(5000);
+server.listen(5000, () => {
+  console.log("server listening");
+});
