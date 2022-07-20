41b2879e2856716c6a012ed9f6b85498ab10f830
gilbert-kiana <gilbertintabo@gmail.com>
2022-07-20 17:22:27 +0300

    apis

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
 
-app.use(express.static("./public"));
+const { products } = require("./data");
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>Homepage</h1><a href ='/api/products'>products</a>");
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products", (req, res) => {
+  console.log(req);
+  const newProduct = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProduct);
 });
 
 app.listen(5000, () => {
-  console.log("server listening on port 5000 ...");
+  console.log("server is listening on port5000");
 });
