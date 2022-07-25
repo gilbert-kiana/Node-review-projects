64a9fc3fd2e9f67ec8df5fa455f3082516e35e50
gilbert-kiana <gilbertintabo@gmail.com>
2022-07-25 22:34:11 +0300

    mongo and moongoose connection established

--- node-express-course/03-task-manager/starter/db/connect.js
+++ node-express-course/03-task-manager/starter/db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://gilbert:<password>@cluster0.28gau.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://gilbert:YHTgJF8Afs7L7HDv@cluster0.28gau.mongodb.net/task_manager?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to the db ..."))
+  .catch((err) => console.log(err));
