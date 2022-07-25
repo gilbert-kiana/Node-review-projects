e73beec7b777cf2e271779e76c6e925d31592561
gilbert-kiana <gilbertintabo@gmail.com>
2022-07-25 21:48:06 +0300

    Working with mongo atlas and building task manager api

--- node-express-course/03-task-manager/starter/app.js
+++ node-express-course/03-task-manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Hello there");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server listening on port ${port}`));
---
+++ node-express-course/03-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://gilbert:<password>@cluster0.28gau.mongodb.net/?retryWrites=true&w=majority";
